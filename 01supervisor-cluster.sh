f4bb91098ee9063cce113765823437eae874e3ce
Craig J Smith <nctiggy@gmail.com>
2022-06-24 19:58:59 -0700

    Update 01supervisor-cluster.sh

--- 01supervisor-cluster.sh
+++ 01supervisor-cluster.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+POWDER_BLUE=$(tput setaf 153)
+MAGENTA=$(tput setaf 5)
+NORMAL=$(tput sgr0)
+GREEN=$(tput setaf 2)
+
 #Make sure we are targeting the supervisor cluster
 kubectl config use-context piper
 
 #create the guest cluster for cnvrg
 kubectl apply -f tkc.yaml
 
-printf "Provisioning Guest Cluster..."
+printf "${POWDER_BLUE}Provisioning Guest Cluster..."
 until [[ -n $(kubectl get tkc cnvrg-cluster -o=jsonpath={.status.phase}) ]]
 do
-  printf "."
+  printf "${NORMAL}."
+  sleep 1m
+done
+printf "${GREEN}Init Complete!\n"
+printf "${POWDER_BLUE}Creating Nodes"
+until [[ $(kubectl get tkc cnvrg-cluster -o=jsonpath={.status.phase}) == "running" ]]
+do
+  printf "${NORMAL}."
   sleep 1m
 done
 
-printf "Done!\n"
-printf "Run script 02 now\n"
+printf "${GREEN}Cluster Complete!\n\n"
+printf "${MAGENTA}Run script 02 now\n"
