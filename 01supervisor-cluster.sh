a9c4b93889501acc7423e9913f989764842c3010
Craig J Smith <nctiggy@gmail.com>
2022-06-24 19:48:29 -0700

    Update 01supervisor-cluster.sh

--- 01supervisor-cluster.sh
+++ 01supervisor-cluster.sh
@@ -2,8 +2,7 @@
 
 #Make sure we are targeting the supervisor cluster
 kubectl config use-context piper
-#Create the vmclass for cnvrg
-kubectl apply -f vmclass.yaml
+
 #create the guest cluster for cnvrg
 kubectl apply -f tkc.yaml
 
