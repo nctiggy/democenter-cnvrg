578a7b3fd10cc6cb7b60a7e61ff0e16cad6dbf12
Craig Smith <nctiggy@gmail.com>
2022-06-24 18:35:39 -0700

    added check for cluster readiness

--- 01supervisor-cluster.sh
+++ 01supervisor-cluster.sh
@@ -6,3 +6,12 @@ kubectl config use-context piper
 kubectl apply -f vmclass.yaml
 #create the guest cluster for cnvrg
 kubectl apply -f tkc.yaml
+
+printf "Provisioning Guest Cluster...
+until $(kubectl get tkc tkc-cluster-01 -o=jsonpath={.status.phase}) == "running"
+do
+  printf "."
+  sleep 1m
+done
+
+printf "Done!\n"
